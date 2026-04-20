285720d2465f35008f79f042501e5a42b4512480
AndreVictorDmr <41845560+AndreVictordmr@users.noreply.github.com>
2026-04-20 16:31:54 -0300

    Criaçao do util e finalizameto da fichaServico

--- src/service/fichaServico.ts
+++ src/service/fichaServico.ts
@@ -1,5 +1,6 @@
 import fs from 'fs';
 import {PATH} from '../config/path'; 
+import { UTIL } from "../util";
 
 interface Ficha{
   id:number;
@@ -7,20 +8,87 @@ interface Ficha{
   atributos?: Record<string,number>;
   habilidades?:Record<string,string>;
 }
-class fichaServico{
+export class fichaServico{
   private bd:Ficha[]=[];
   constructor(){
     const dados = fs.readFileSync(PATH.db_path, 'utf-8');
     this.bd = JSON.parse(dados||"[]");
   }
-  creat(ficha:Ficha){
-    // passo algum codigo do controll par verificaçao 
+  private salvarNoArquivo() {
+    fs.writeFileSync(PATH.db_path, JSON.stringify(this.bd, null, 2));
   }
-  editar(id:number,dado:Partial<Ficha>){
-    // passo algum codigo do controll par verificaçao   
+  criar(ficha:Ficha):Ficha{
+    const verificaId = UTIL.validarNumeros(ficha.id);
+    const verificaNome = UTIL.validarTexto(ficha.nome);
+    if(verificaId && verificaNome ){
+            if (ficha.atributos !== undefined) {
+        for( const atributo in ficha.atributos){
+          const valor = ficha.atributos[atributo];
+          if(!UTIL.validarNumeros(valor)) throw new Error(`${atributo} com valor invalido`);
+        }
+      }
+      if (ficha.habilidades !== undefined) {
+        for( const skill in ficha.habilidades){
+          const valor = ficha.habilidades[skill];
+          if(!UTIL.validarTexto(valor)) throw new Error(`${skill} com valor invalido`);
+        }
+      }
+      
+      const novaEntrada = this.bd.some(f => f.id === ficha.id);
+      if(novaEntrada){
+        throw new Error("ID já cadastrado!");
+      }else{
+        this.bd.push(ficha);
+        this.salvarNoArquivo();
+        return ficha;
+      }
+    }else{
+        throw new Error("Dados invalidos");
+    }
   }
+  editar(id:number,dado:Partial<Ficha>){  
+    const verificaId = UTIL.validarNumeros(id);
+    if(!verificaId) throw new Error("Id não valido");
+      const entradaId= this.bd.findIndex(f=>f.id===id);
+      if(entradaId === -1 ) throw new Error("Ficha não encontrada");
+      
+      if (dado.nome !== undefined) {
+        if(!UTIL.validarTexto(dado.nome)) throw new Error("Nome invalido");
+      }
+      if (dado.atributos !== undefined) {
+        for( const atributo in dado.atributos){
+          const valor = dado.atributos[atributo];
+          if(!UTIL.validarNumeros(valor)) throw new Error(`${atributo} com valor invalido`);
+        }
+      }
+      if (dado.habilidades !== undefined) {
+        for( const skill in dado.habilidades){
+          const valor = dado.habilidades[skill];
+          if(!UTIL.validarTexto(valor)) throw new Error(`${skill} com valor invalido`);
+        }
+      }
+      
+    this.bd[entradaId] = { 
+      ...this.bd[entradaId], 
+      ...dado,
+    
+      id: this.bd[entradaId].id 
+    };
+
+    this.salvarNoArquivo();
+    return this.bd[entradaId];
+  }
+  
+  
   selecionarPorId(id:number):Ficha|undefined{
-    // passo algum codigo do controll par verificaçao
+    const verificaId = UTIL.validarNumeros(id);
+    if( verificaId ){
+      const busca = this.bd.find(f=> f.id === id)
+      if(busca === undefined) throw new Error("Id não registrado");
+      return busca;
+    }else{
+      throw new Error("Id invalido");
+    }
   }
 
 }
--- src/util.ts
+++ src/util.ts
@@ -1,3 +1,13 @@
-export validarTexto(valor:string): boolean{
-  
+export function validarTexto(texto:string): boolean{
+    if(!texto) return false;
+    const textoValido = texto.trim();
+    if(texto.length === 0) return false;
+    const caracterPermitidos = /^[a-zA-Z0-9À-ÿ\s]+$/;
+    return caracterPermitidos.test(textoValido);
+}
+
+export function validarNumeros(valor:number):boolean{
+    if(isNaN(valor)) return false;
+    if(valor == null|| valor == undefined) return false;
+    return valor>=0;
 }
