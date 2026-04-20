ed2b60f54e36f682b25e80a3b9a18d1559376f03
AndreVictorDmr <41845560+AndreVictordmr@users.noreply.github.com>
2026-04-19 22:29:49 -0300

    Initialize database in fichaServico constructor
    Added constructor to initialize database from file.

--- src/service/fichaServico.ts
+++ src/service/fichaServico.ts
@@ -1,3 +1,5 @@
+import fs from 'fs';
+import {PATH} from '../config/path'; 
 
 interface Ficha{
   id:number;
@@ -6,17 +8,19 @@ interface Ficha{
   habilidades?:Record<string,string>;
 }
 class fichaServico{
-  private bd : Ficha[]=[];
-  
+  private bd:Ficha[]=[];
+  constructor(){
+    const dados = fs.readFileSync(PATH.db_file, 'utf-8');
+    this.bd = JSON.parse(dados||"[]");
+  }
   creat(ficha:Ficha){
     // passo algum codigo do controll par verificaçao 
   }
-  editar(id:number,ficha:array,dado:ficha){
+  editar(id:number,dado:Partial<Ficha>){
     // passo algum codigo do controll par verificaçao   
   }
   selecionarPorId(id:number):Ficha|undefined{
     // passo algum codigo do controll par verificaçao
-    .fecht();
   }
 
 }
